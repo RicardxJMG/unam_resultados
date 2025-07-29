7add7a7d460a7b8bd2d380c2aec64ba31c80e39d
Ricardo <r.x.mg1801@gmail.com>
2025-07-29 02:56:00 -0600

    scores scrapping modified

--- scrapping_puntaje.py
+++ scrapping_puntaje.py
@@ -1,8 +1,10 @@
+#%%
 import pandas as pd 
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
+from bs4 import BeautifulSoup, Tag 
 
 
 def init_driver(driver_path: str = "C:/WebDrivers/chromedriver.exe") -> webdriver.Chrome | None:
@@ -26,11 +28,7 @@ def get_main_content(driver: webdriver.Chrome, url: str) -> webdriver.Chrome:
 
     return main_content
 
-def get_scores_from_school(scores_table: webdriver.Chrome, school_name: str, career_name: str, area: int) -> pd.DataFrame:
-    
-    id_list: list[str] = []
-    score_list: list[str | int] = []
-    accepted_list: list[str] = []
+def get_scores_from_school(scores_table: BeautifulSoup, career_id: str, school_id: str, area: int) -> pd.DataFrame:
     
     acreditado_dict: dict = {
                 'S': 'Seleccionado',
@@ -42,74 +40,114 @@ def get_scores_from_school(scores_table: webdriver.Chrome, school_name: str, car
     area_dict: dict = {1: 'A1', 2: 'A2', 3: 'A3', 4: 'A4'}
     
     try: 
-        rows = scores_table.find_elements(by = By.TAG_NAME, value = 'tr')
-        for row in rows:  
-            cells = row.find_elements(by = By.TAG_NAME, value = 'td')
-            id_list.append(cells[0].text)
-            score_list.append(cells[1].text)
-            accepted_list.append(acreditado_dict[cells[2].text])
         
-        career_list: list[str] = [career_name]*len(id_list)
-        school_list: list[str] = [school_name]*len(id_list)
-        area_list: list[str] = [area_dict[area]]*len(id_list)
+        header_table = [th.text.strip() for th in scores_table.find('thead').find_all('th')]
+        scores_data = scores_table.find('tbody').find_all('tr')
+        table_data = [[td.text.strip() for td in row.find_all('td')] for row in scores_data]
         
-        data: dict = {
-            'id_aspirante': id_list,
-            'escuela': school_list,
-            'carrera': career_list,
-            'area': area_list,
-            'puntaje': score_list,
-            'acreditado': accepted_list
-        }
+        df_scores = pd.DataFrame(columns=header_table, data = table_data)
         
-        print(f'>>> Resultados obtenidos de la escuela {school_name} para la carrera {career_name} del área {area_dict[area]}')
-        return pd.DataFrame(data)
+        df_scores = df_scores.drop(columns =['Detalles', 'Diagnóstico'], axis =1)
+        rename_cols = {'Número de comprobante': 'id_aspirante',
+                       'Aciertos': 'puntaje',
+                       'Acreditado': 'acreditado'}
+        
+        rows, _ = df_scores.shape
+        
+        df_scores.rename(columns=rename_cols, inplace=True)
+        df_scores['acreditado'] = df_scores['acreditado'].map(acreditado_dict)
+        
+        df_scores['id_carrera'] = [career_id]*rows
+        df_scores['id_escuela'] = [school_id]*rows
+        df_scores['id_area'] = [area_dict[area]]*rows
+        
+        print(df_scores.sample(3))
+        
+        
+
+        print(f'>>> Resultados obtenidos de la escuela {school_id} para la carrera {career_id} del área {area_dict[area]}')
+        
+        return df_scores
 
     except Exception as e:
         print(f"Error processing scores table: {e}")
-        return pd.DataFrame(columns=['id_aspirante', 'escuela', 'carrera','area', 'puntaje', 'acreditado'])
+        return pd.DataFrame(columns=['id_aspirante','id_area', 'id_escuela', 'id_carrera', 'puntaje', 'acreditado'])
 
 
-def get_scores_career(driver: webdriver.Chrome, schools: webdriver.Chrome, career_name: str, area: int) -> pd.DataFrame:
-    df_career_scores = pd.DataFrame(columns=['id_aspirante', 'escuela','area', 'carrera', 'puntaje', 'acreditado'])
+def get_scores_career(driver: webdriver.Chrome, schools: list[webdriver.Chrome], career_id: str, area: int) -> pd.DataFrame:
+    df_career_scores = pd.DataFrame(columns=['id_aspirante','id_area', 'id_escuela', 'id_carrera', 'puntaje', 'acreditado'])
+
+    school_info: list[list] = list() # what should I do with this?....
     try:
         for school in schools:
-            school_name: str = school.text
+            
+            school_name: str = school.text.strip()
+            school_id:str = school.get_attribute('href')[-10:-5]
+            school_info.append([school_name, school_id])
+            
             school.click()
-            print(f">> Obteniendo resultados de la carrera: {career_name} en la escuela: {school_name}")
+            print(f">> Obteniendo resultados de la carrera: {career_id} en la escuela: {school_name}")
+            score_table_bs = BeautifulSoup(driver.find_element(by=By.TAG_NAME , 
+                                                                value = 'table').get_attribute('outerHTML'), 'html.parser')
+            # print(score_table_bs)
             
             df_career_scores = pd.concat([df_career_scores,
-                                            get_scores_from_school(driver.find_element(by = By.TAG_NAME, value='tbody'),
-                                            school_name, career_name, area)],
+                                            get_scores_from_school(score_table_bs,
+                                            school_id, career_id, area)],
                                             ignore_index=True)
             driver.back() # go back to A(i) list
     
     except Exception as e: 
-        print(f"Error processing career {career_name}: {e}")
-        return df_career_scores
+        print(f"Error processing career {career_id}: {e}")
+        raise
+    
     return df_career_scores
 
     
-def get_scores_area(driver: webdriver.Chrome, area_page: list[str], area: int) -> pd.DataFrame:    
-    df_area_scores = pd.DataFrame(columns=['id_aspirante', 'escuela','area', 'carrera', 'puntaje', 'acreditado'])
+def get_scores_area(driver: webdriver, area_page: list[webdriver.Chrome], area: int) -> pd.DataFrame:    
+
+    df_area_scores = pd.DataFrame(columns=['id_aspirante','id_area', 'id_escuela', 'id_carrera', 'puntaje', 'acreditado'])
+    
+    schools_info: dict[str, set] = {
+        'id_plantel': set(), 
+        'plantel': set()
+    } 
+    
+    carrera_info: dict[str, set] = {
+        #this dictionary storage info of each career
+        'id_carrera': set(),
+        'id_area': set(),
+        'carrera': set()
+    }
+    
     try:
         for career in area_page:
-            career_name = career.find_element(by=By.TAG_NAME, value='h3').text
-            schools = career.find_elements(by=By.TAG_NAME, value='a')
-            print(f"> Accediendo a los resultados de la carrera: {career_name}")
+            career_bs = BeautifulSoup(career.get_attribute('outerHTML'), 'html.parser')
+            career_name = career_bs.find('h3').text.strip()
+            
+            schools_bs = career_bs.find_all('a')
 
+            career_id = schools_bs[0]['href'][2:5]
+            
+            carrera_info['id_area'].add(career_id) 
+            carrera_info['id_area'].add(f'A{area}')
+            carrera_info['carrera'].add(career_name)   
+            # schools_id.append(a_href['href'][5:10] for a_href in href_ids)
+                    
+            schools = career.find_elements(by=By.TAG_NAME, value='a')
+            
             df_area_scores = pd.concat([df_area_scores, 
-                                        get_scores_career(driver, schools, career_name, area)],
+                                        get_scores_career(driver, schools, career_id, area)],
                                         ignore_index=True)
-            print()
     except Exception as e:
-        print(f"Error processing area {area}: {e}")
-        return df_area_scores
-    
+        print(f"\n> Error processing area {area}: {e}")
+        driver.quit()
+        raise
+        
     return df_area_scores
 
 def get_scores(pages: list[str]) -> pd.DataFrame:
-    df_area_scores = pd.DataFrame(columns=['id_aspirante', 'escuela','area', 'carrera', 'puntaje', 'acreditado'])
+    df_area_scores = pd.DataFrame(columns=['id_aspirante', 'id_escuela','id_area', 'id_carrera', 'puntaje', 'acreditado'])
     driver = init_driver()
     if driver is not None:
         for area,page in enumerate(pages):
@@ -128,17 +166,29 @@ def get_scores(pages: list[str]) -> pd.DataFrame:
     
     driver.quit()
     return df_area_scores
+
+
 #%%
 if __name__ == "__main__":
-
-    pages = [f'https://www.dgae.unam.mx/Licenciatura2025/resultados/{i}5.html' for i in range(1,5)]    
-    datos_path = './datos'
-    df_area_scores = get_scores(pages)
-    if not df_area_scores.empty:
-        os.makedirs(datos_path, exist_ok=True)
-        ruta = os.path.join(datos_path, 'resultados_unam_2025.csv')
-        df_area_scores.to_csv(ruta, index=False)
-        print(f"Scrapping completado :D\n Tamaño del DataFrame: {df_area_scores.shape}")
+    # Testeando 
+    pages = [f'https://www.dgae.unam.mx/Licenciatura2025/resultados/{i}5.html' for i in range(1,5)]  
+    page_test = pages[0]
+    
+    df_area_scores = pd.DataFrame(columns=['id_aspirante', 'id_escuela','id_area', 'carrera', 'puntaje', 'acreditado'])
+    driver = init_driver()
+    area = 0
+    if driver is not None:
         
+        print(f'\n::::: Obteniendo datos de las carreras del área {1} :::::\n')
+        main_content = get_main_content(driver, page_test)
+        if main_content is not None:
+            career_content = main_content.find_elements(by=By.CLASS_NAME, value='post-preview')
+            df_area_scores = pd.concat([df_area_scores,
+                                        get_scores_area(driver, career_content, area+1)],
+                                        ignore_index=True)
+            
+        else:
+            print(f'No main content found for {page_test}')
     else:
-        print("No se pudo hacer el scrapping.")
+        print('Driver initialization failed')
+    
